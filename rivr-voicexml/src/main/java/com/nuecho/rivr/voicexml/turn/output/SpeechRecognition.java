17892f2e3d2e03e7d11a97d15244611ec9a8787b
Jean-Philippe Gariépy <Jean-Philippe.Gariepy@nuecho.com>
2018-02-20 09:06:59 -0500

    SpeechRecognition.java: Fixed hashCode and equals method: now correctly invokes methods from super.

--- rivr-voicexml/src/main/java/com/nuecho/rivr/voicexml/turn/output/SpeechRecognition.java
+++ rivr-voicexml/src/main/java/com/nuecho/rivr/voicexml/turn/output/SpeechRecognition.java
@@ -188,12 +188,12 @@ public final class SpeechRecognition extends Recognition {
     @Override
     public int hashCode() {
         final int prime = 31;
-        int result = 1;
+        int result = super.hashCode();
         result = prime * result + (mCompleteTimeout == null ? 0 : mCompleteTimeout.hashCode());
-        result = prime * result + (mMaxNBest == null ? 0 : mMaxNBest.hashCode());
-        result = prime * result + (mIncompleteTimeout == null ? 0 : mIncompleteTimeout.hashCode());
-        result = prime * result + (mMaxSpeechTimeout == null ? 0 : mMaxSpeechTimeout.hashCode());
         result = prime * result + (mConfidenceLevel == null ? 0 : mConfidenceLevel.hashCode());
+        result = prime * result + (mIncompleteTimeout == null ? 0 : mIncompleteTimeout.hashCode());
+        result = prime * result + (mMaxNBest == null ? 0 : mMaxNBest.hashCode());
+        result = prime * result + (mMaxSpeechTimeout == null ? 0 : mMaxSpeechTimeout.hashCode());
         result = prime * result + (mSensitivity == null ? 0 : mSensitivity.hashCode());
         result = prime * result + (mSpeedVersusAccuracy == null ? 0 : mSpeedVersusAccuracy.hashCode());
         return result;
@@ -202,24 +202,24 @@ public final class SpeechRecognition extends Recognition {
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
-        if (obj == null) return false;
+        if (!super.equals(obj)) return false;
         if (getClass() != obj.getClass()) return false;
         SpeechRecognition other = (SpeechRecognition) obj;
         if (mCompleteTimeout == null) {
             if (other.mCompleteTimeout != null) return false;
         } else if (!mCompleteTimeout.equals(other.mCompleteTimeout)) return false;
-        if (mMaxNBest == null) {
-            if (other.mMaxNBest != null) return false;
-        } else if (!mMaxNBest.equals(other.mMaxNBest)) return false;
-        if (mIncompleteTimeout == null) {
-            if (other.mIncompleteTimeout != null) return false;
-        } else if (!mIncompleteTimeout.equals(other.mIncompleteTimeout)) return false;
-        if (mMaxSpeechTimeout == null) {
-            if (other.mMaxSpeechTimeout != null) return false;
-        } else if (!mMaxSpeechTimeout.equals(other.mMaxSpeechTimeout)) return false;
         if (mConfidenceLevel == null) {
             if (other.mConfidenceLevel != null) return false;
         } else if (!mConfidenceLevel.equals(other.mConfidenceLevel)) return false;
+        if (mIncompleteTimeout == null) {
+            if (other.mIncompleteTimeout != null) return false;
+        } else if (!mIncompleteTimeout.equals(other.mIncompleteTimeout)) return false;
+        if (mMaxNBest == null) {
+            if (other.mMaxNBest != null) return false;
+        } else if (!mMaxNBest.equals(other.mMaxNBest)) return false;
+        if (mMaxSpeechTimeout == null) {
+            if (other.mMaxSpeechTimeout != null) return false;
+        } else if (!mMaxSpeechTimeout.equals(other.mMaxSpeechTimeout)) return false;
         if (mSensitivity == null) {
             if (other.mSensitivity != null) return false;
         } else if (!mSensitivity.equals(other.mSensitivity)) return false;
